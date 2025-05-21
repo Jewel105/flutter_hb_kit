1e1c7e634e4139b95dc405a813db3c9a7b28ba99
Jewel <ylingzhu2022@gmail.com>
2025-05-21 13:59:37 +0800

    fix:去掉调试

--- packages/hb_rename/lib/command/hb_rename_command.dart
+++ packages/hb_rename/lib/command/hb_rename_command.dart
@@ -1,5 +1,8 @@
 import 'package:args/command_runner.dart';
-import 'package:hb_rename/util/_android_tool.dart';
+
+import '../util/_android_tool.dart';
+import '../util/_ios_tool.dart';
+import '../util/_lib_tool.dart';
 
 // dart run hb_rename all -a MyApp -b com.example.myapp
 class HbRenameCommand extends Command {
@@ -38,7 +41,7 @@ class HbRenameCommand extends Command {
       throw ArgumentError('Both app-name and blundle-id are required.');
     }
     AndroidTool.resetApp(appName, bundleId);
-    // IosTool.resetApp(appName, bundleId);
-    // LibTool.resetApp(appName);
+    IosTool.resetApp(appName, bundleId);
+    LibTool.resetApp(appName);
   }
 }
