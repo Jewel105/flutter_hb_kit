727cccb2b4373f55f0ace9ad49b6a42b35ebce78
Jewel <ylingzhu2022@gmail.com>
2025-05-26 16:08:52 +0800

    feat: 全屏显示二维码

--- packages/hb_qr/lib/widget/hb_qr_box.dart
+++ packages/hb_qr/lib/widget/hb_qr_box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class HbQrBox extends StatelessWidget {
@@ -8,12 +9,28 @@ class HbQrBox extends StatelessWidget {
   const HbQrBox({super.key, required this.data, this.size = 200});
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: size,
-      child: QrImageView(
-        data: data,
-        size: size,
-        padding: const EdgeInsets.all(6),
+    return InkWell(
+      onTap: () {
+        // 全屏显示二维码
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Center(
+              child: Container(
+                color: Colors.white,
+                child: HbQrBox(data: data, size: 0.9.sw),
+              ),
+            );
+          },
+        );
+      },
+      child: SizedBox(
+        width: size,
+        child: QrImageView(
+          data: data,
+          size: size,
+          padding: const EdgeInsets.all(6),
+        ),
       ),
     );
   }
