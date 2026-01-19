d9f08cc75465601d9fe3deb99f1509171a4bc841
Jewel <ylingzhu2022@gmail.com>
2026-01-19 14:56:57 +0800

    chore: remove chart widget

--- packages/hb_chart/lib/hb_chart.dart
+++
@@ -1,9 +0,0 @@
-library hb_chart;
-
-export 'chart_style.dart';
-export 'depth_chart.dart';
-export 'entity/index.dart'; //数据
-export 'extension/num_ext.dart';
-export 'k_chart_widget.dart'; //布局
-export 'renderer/index.dart';
-export 'utils/index.dart'; //数据加工
